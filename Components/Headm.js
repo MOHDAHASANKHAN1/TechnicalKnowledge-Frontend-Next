4f08754ffcbb06a520573eb269260a5d6db0c4d0
MOHDAHASANKHAN1 <95666818+MOHDAHASANKHAN1@users.noreply.github.com>
2022-12-22 15:59:56 +0530

    Update Headm.js

--- Components/Headm.js
+++ Components/Headm.js
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
+import Head from "next/head";
+import { useRouter } from "next/router";
 
 export default function Headm(props) {
   const { asPath } = useRouter();
@@ -9,14 +9,27 @@ export default function Headm(props) {
         <title>{props.tittle}</title>
         <meta name="description" content={props.description} />
         <meta name="keywords" content={props.keywords} />
-        <link rel="sitemap" href="https://technicalknowledge.herokuapp.com/sitemap.xml" type="application/xml" />
-        <link rel="canonical" href={"https://technicalknowledge.herokuapp.com" + asPath} />
+        <link
+          rel="sitemap"
+          href="https://technicalknowledge.vercel.app//sitemap.xml"
+          type="application/xml"
+        />
+        <link
+          rel="canonical"
+          href={"https://technicalknowledge.vercel.app/" + asPath}
+        />
         <meta property="article:tag" content={props.keywords} />
         <meta property="og:title" content={props.tittle} />
         <meta property="og:description" content={props.description} />
-        <meta property="og:url" content={"https://technicalknowledge.herokuapp.com" + asPath} />
+        <meta
+          property="og:url"
+          content={"https://technicalknowledge.vercel.app/" + asPath}
+        />
         <meta property="og:site_name" content="Technical Knowledge" />
-        <meta property="article:publisher" content="https://www.facebook.com/people/Mo-Ahsan-Khan/100043795833453/" />
+        <meta
+          property="article:publisher"
+          content="https://www.facebook.com/people/Mo-Ahsan-Khan/100043795833453/"
+        />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="article"></meta>
         <meta property="og:image" content={props.imgurl} />
@@ -28,8 +41,15 @@ export default function Headm(props) {
         <meta name="twitter:image" content={props.imgurl} />
         <meta name="twitter:creator" content="@MoAhsanKhan5" />
         <meta name="author" content="Mohd Ahasan Khan" />
-        <meta name="google-site-verification" content="qdtQbk2SFCSNiz3XDJw1dAD4Cy4hWXUTuFiYKh-yir0" />
-        <link rel="icon" type="image/png" href="https://technicalknowledge.herokuapp.com/Icon.png"></link>
+        <meta
+          name="google-site-verification"
+          content="qdtQbk2SFCSNiz3XDJw1dAD4Cy4hWXUTuFiYKh-yir0"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          href="https://technicalknowledge.vercel.app//Icon.png"
+        ></link>
       </Head>
     </>
   );
